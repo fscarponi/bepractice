760e5705404f9ed224b4cc95f62ca096831487db
fscarponi <fscarponi@gmail.com>
2022-12-15 20:34:16 +0100

    building dependencies, fix junit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.serialization") version "1.7.22"
+
     application
 }
 
@@ -10,13 +12,37 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap")//access to eap for 2.2.1
+
 }
+val ktorVersion: String by project
+val logbackVersion: String by project
+val kotlinVersion: String by project
+val kotlinxSerializationVersion: String by project
 
 dependencies {
-    testImplementation(kotlin("test"))
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    //ktor
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    //tests
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation(kotlin("test-junit5"))
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
---
+++ src/test/kotlin/BasicTests.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class ApplicationTest {
+
+
+    @Test
+    fun dummyTest() {
+        //test for junit 5 -> this should never fail!
+        assertTrue(true)
+    }
+
+
+
+}
